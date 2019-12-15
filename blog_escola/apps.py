3d936e0bd5ff14c5707c808174d323119b19e145
JhonnyLin <victor.everlen@gmail.com>
2019-12-15 18:27:47 -0300

    Adicionando a aplicação template html e views

---
+++ blog_escola/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlogEscolaConfig(AppConfig):
+    name = 'blog_escola'
---
+++ blog_escola/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# render funciona como um atalho pegando a pagian no templates e passando para HttpResponde
+from django.http import HttpResponse
+
+def home(request):
+    # os 3 parametros são requisição, pagina e um texto, que no caso é um dicionario
+    return render(request, 'home.html', {'usuario' : 'Lin'} )
